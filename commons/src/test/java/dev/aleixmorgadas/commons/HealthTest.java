7a1327bee8e775af436b910c8d03df1007287e37
Aleix Morgadas <hello@aleixmorgadas.dev>
2023-01-14 13:41:33 +0100

    preparing the project

---
+++ commons/src/test/java/dev/aleixmorgadas/commons/HealthTest.java
@@ -0,0 +1,13 @@
+package dev.aleixmorgadas.commons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HealthTest {
+
+    @Test
+    void healthTest() {
+        assertTrue(true);
+    }
+}
---
+++ domain-service-level/src/test/java/dev/aleixmorgadas/domainservicelevel/ApplicationServiceTest.java
@@ -0,0 +1,14 @@
+package dev.aleixmorgadas.domainservicelevel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ApplicationServiceTest {
+
+    @Test
+    void test() {
+        assertThat(true).isTrue();
+    }
+}
